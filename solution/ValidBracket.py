89aa8d6f333c688bab71c25070e5afed0d5535fd
Vergilgeek <251627752@qq.com>
2020-08-14 09:45:17 +0800

    20. 有效的括号

--- solution/ValidBracket.py
+++ solution/ValidBracket.py
@@ -1,29 +1,35 @@
 
- # 20. 有效的括号
- # 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- # <p>
- # 有效字符串需满足：
- # <p>
- # 左括号必须用相同类型的右括号闭合。
- # 左括号必须以正确的顺序闭合。
- # 注意空字符串可被认为是有效字符串。
- 
+# 20. 有效的括号
+# 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+# <p>
+# 有效字符串需满足：
+# <p>
+# 左括号必须用相同类型的右括号闭合。
+# 左括号必须以正确的顺序闭合。
+# 注意空字符串可被认为是有效字符串。
+
 def solution(s: str) -> bool:
-    stack =[]
-    for char in s:
-        if char == '(':
-            stack.append(')')
-        elif char == '[':
-            stack.append(']')
-        elif char == '{':
-            stack.append('}')
-        elif len(stack)==0 or stack.pop() != char:
-            return False
-    if len(stack) != 0:
+    if len(s) % 2 == 1:
         return False
-    return True
+
+    pairs = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    }
+    stack = list()
+    for c in s:
+        if c in pairs:
+            if not stack or stack[-1] != pairs[c]:
+                return False
+            stack.pop()
+        else:
+            stack.append(c)
+    return not stack
+
 
 def main():
     print(solution("["))
+
 
 main()
